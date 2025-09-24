c489b21381548cd75444af75afe5d4d5c53d3fd3
Adrian <adrian@theround.com>
2025-09-25 00:11:59 +0100

    chore: initialise Vite + Tailwind + Jest + Firebase scaffolding

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.(ts|tsx)',
+    '<rootDir>/src/**/*.(test|spec).(ts|tsx)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.(ts|tsx)',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/vite-env.d.ts'
+  ]
+};
